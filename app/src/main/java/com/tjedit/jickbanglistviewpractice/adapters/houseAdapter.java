e30fe5654eaccdc04ac509524b7fb9cd5f9889bb
user <kyronko@naver.com>
2019-04-21 15:07:40 +0900

    메인화면에서의 상세설명쪽에  패딩을 줌

--- app/src/main/java/com/tjedit/jickbanglistviewpractice/adapters/houseAdapter.java
+++ app/src/main/java/com/tjedit/jickbanglistviewpractice/adapters/houseAdapter.java
@@ -2,18 +2,47 @@ package com.tjedit.jickbanglistviewpractice.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.tjedit.jickbanglistviewpractice.R;
+import com.tjedit.jickbanglistviewpractice.datas.Data;
 
 import java.util.List;
 
-public class houseAdapter extends ArrayAdapter <houseAdapter> {
+public class HouseAdapter extends ArrayAdapter <Data> {
     Context mContext;
-    List<houseAdapter> mList;
+    List<Data> mList;
     LayoutInflater inf;
-    public houseAdapter(Context context, List<houseAdapter>list){
+    public HouseAdapter(Context context, List<Data>list){
         super(context, R.layout.list_item,list);
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
 
+
+    }
+
+
+    @Override
+    public View getView(int position,  View convertView,  ViewGroup parent) {
+        View row = convertView;
+        if(row==null)
+        {
+            row = inf.inflate(R.layout.list_item,null);
+        }
+        Data houseData = mList.get(position);
+        TextView houseMy = row.findViewById(R.id.houseMy);
+        TextView houseFloor = row.findViewById(R.id.houseFloor);
+        TextView houseAddress = row.findViewById(R.id.houseAddress);
+
+        houseMy.setText(houseData.houseMy);
+        houseFloor.setText(String.format("%d 층",houseData.houseFloor));
+        houseAddress.setText(houseData.houseAddress);
+
+
+        return row;
     }
 }
